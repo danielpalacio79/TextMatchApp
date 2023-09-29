b86e656213a6c1a379ecafa392159398a95e1537
danielpalacio79 <111745@tecnicatura.frc.utn.edu.ar>
2023-09-28 21:22:36 -0300

    TestUI

--- app/src/test/java/com/example/textmatchapp/CompararTxtViewModelUnitTest.kt
+++ app/src/test/java/com/example/textmatchapp/CompararTxtViewModelUnitTest.kt
@@ -8,8 +8,10 @@ import org.junit.Rule
 import com.example.textmatchapp.viewmodel.CompararTextViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
+import org.junit.After
 import org.junit.Before
 
 /**
@@ -36,5 +38,9 @@ class CompararTxtViewModelUnitTest {
     fun setup(){
         Dispatchers.setMain(dispatcher)
         viewModel = CompararTextViewModel()
+    }
+    @After
+    fun tearDown(){
+        Dispatchers.resetMain()
     }
 }
